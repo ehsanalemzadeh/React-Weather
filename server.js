9ad9cf9fcdbb923cd11125586f5dc35a36412118
Ehsan <ehsan.alemzadeh@gmail.com>
2017-08-15 19:00:34 +0430

    Prepared for Heroku 2

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ var app = express();
 
 const PORT = process.env.port || 8000;
 
-app.use(function(req,res,next){
-  // console.log(req.headers);
-  if(req.headers['x-forwarded-proto'] === 'https' ){
-    req.redirect('http://'+req.hostname+req.url);
-  }else{
-    next();
-  }
-});
+// app.use(function(req,res,next){
+//   // console.log(req.headers);
+//   if(req.headers['x-forwarded-proto'] === 'https' ){
+//     req.redirect('http://'+req.hostname+req.url);
+//   }else{
+//     next();
+//   }
+// });
 
 app.use(express.static('public'));
 
