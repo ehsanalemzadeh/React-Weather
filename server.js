831152bac96580f5402a3fb36faba6629e3fd9d3
Ehsan <ehsan.alemzadeh@gmail.com>
2017-08-15 18:36:20 +0430

    Prepared for Heroku

--- server.js
+++ server.js
@@ -3,8 +3,18 @@ var express = require('express');
 // Create our app
 var app = express();
 
+const PORT = process.env.port || 8000;
+
+app.use(function(req,res,next){
+  if(req.headers['x-forward-proto'] === 'http' ){
+    next();
+  }else{
+    req.redirect('http://'+req.hostname+req.url);
+  }
+});
+
 app.use(express.static('public'));
 
-app.listen(8000, function () {
+app.listen(PORT, function () {
   console.log('Express server is up on port 8000');
 });
