ecd95d2d0f17f875b8f837b4adfd7716b44b45ca
Ehsan <ehsan.alemzadeh@gmail.com>
2017-08-15 19:03:06 +0430

    Prepared for Heroku 2

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 // Create our app
 var app = express();
 
-const PORT = process.env.port || 8000;
+const PORT = process.env.PORT || 8000;
 
 // app.use(function(req,res,next){
 //   // console.log(req.headers);
